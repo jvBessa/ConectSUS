57a99c6b5dba37421d4b37864690363ba4f17eb0
João Victor Bessa de Paula <jvbpaula@gmail.com>
2022-05-23 17:28:19 -0300

    Adding more programation
    Very very hard, almost giving up lol

--- script.js
+++ script.js
@@ -5,6 +5,19 @@ var fila = document.querySelector(".fila");
 var ped = document.querySelector("#ped")
 var clinico = document.querySelector("#cG");
 var tria = document.querySelector("#triagem");
+var hospital = "none";
+
+
+var hospitalAtual = document.querySelector("#hospitalAtual");
+
+
+var dataAtend = document.querySelector("#date");
+var datas = ['22/06/2022', '24/06/2022', '28/06/2022', '04/07/2022', '10/07/2022'];
+var doutores = ['Dr. André', 'Dra. Carla', 'Dr, João', 'Dr. Caio', 'Dra. Laura'];
+var dataIndex = 0;
+dataAtend.textContent = datas[dataIndex];
+
+
 
 function mudarBG1(){
     agendamento.style.backgroundColor = "Gray";
@@ -37,4 +50,48 @@ function triagem(){
     clinico.style.border = "none";
     ped.style.border = "none";
 }
+
+function hA(){
+    hospital = "Hospital A";
+    console.log(hospital);
+    sessionStorage.setItem("hospital", hospital);
+}
+function hB(){
+    hospital = "Hospital B";
+    console.log(hospital);
+    sessionStorage.setItem("hospital", hospital);
+}
+function hC(){
+    hospital = "Hospital C";
+    console.log(hospital);
+    sessionStorage.setItem("hospital", hospital);
+}
+function hD(){
+    hospital = "Hospital D";
+    console.log(hospital);
+    sessionStorage.setItem("hospital", hospital);
+}
+function hE(){
+    hospital = "Hospital E";
+    console.log(hospital);
+    sessionStorage.setItem("hospital", hospital);
+}
+
+hospitalAtual.innerText = sessionStorage.getItem("hospital", hospital);
+console.log(hospitalAtual);
+
+
+function up(){
+    dataIndex++;
+    console.log(datas[dataIndex])
+    dataAtend.textContent = datas[dataIndex];
+}
+function down(){
+    dataIndex--;
+    console.log(datas[dataIndex])
+    dataAtend.textContent = datas[dataIndex];
+}
+
+
+//pagina de conclusão
 
